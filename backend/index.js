d8b25e3fbd672e3a35cfe57d300430dd4d3d2d08
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2024-12-28 19:53:30 +0200

    Test deploy react in back-end

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import { getUser } from './dataAccess/UserDataAccess.js';
 
@@ -11,13 +12,15 @@ dotenv.config();
 export const port = process.env.PORT ? process.env.PORT : 3600;
 
 app.use(express.json());
+app.use(express.static('build'));
 
 app.listen(port, ()=>{
     console.log(`server running on http://localhost:${port}`);
 });
 
 app.get("/", (req, res, next) => {
-    res.status(200).json({"message": "OK"});
+   let path = path.resolve();
+   res.sendFile(path.join(path, "build", "index.html"));
 });
 
 app.use("/user-api", usersRouter);
