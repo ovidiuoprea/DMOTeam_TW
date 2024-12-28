aaf00e5d1718612e5041f23b3e73849bc168c5bb
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2024-12-28 21:08:02 +0200

    POST /user-api/user - Input validation

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -7,6 +7,17 @@ usersRouter.route('/user')
     .get(async (req, res) => { res.status(200).json(await getUser())});
 
 usersRouter.route('/user')
-    .post(async (req, res) => { res.status(201).json(await createUser(req.body, false))});
+    .post(async (req, res) => { 
+        const user = req.body;
+        if(!user || Object.keys(user).length == 0) {
+            return res.status(400).json({"message": "Invalid request"});
+        }
+        if(!user.name || !user.email || !user.password || !user.role) {
+            res.status(400).json({"message": "Invalid user object"});
+        }
+        else {
+            res.status(201).json(await createUser(req.body, false))
+        }
+    });
 
 export default usersRouter;
