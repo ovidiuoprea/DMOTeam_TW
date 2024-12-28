5ea769c9c5cda95282ee42cf4fbe5a169e977191
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2024-12-28 19:32:14 +0200

    Sequelize ORM config

---
+++ backend/dbORMConfig.js
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+
+const dbORM = new Sequelize({
+    dialect: "mysql",
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    logging: false,
+
+    define: {
+        timestamps: false,
+        freezeTableName: true
+    }
+});
+
+export default dbORM;
