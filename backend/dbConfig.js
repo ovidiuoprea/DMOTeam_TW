6503a4193b97dd315f5a902f3352bf1e3324fe6f
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2024-12-28 19:31:58 +0200

    Change MySQL Connection to Pool
    See
    https://sidorares.github.io/node-mysql2/docs#using-promise-wrapper for documentation.
    
    Using createPool instead of createConnection for async / await and promise-based mechanisms

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -2,12 +2,20 @@ import mysql from "mysql2";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const conn = mysql.createConnection({
+// https://sidorares.github.io/node-mysql2/docs#using-promise-wrapper
+const conn = mysql.createPool({
     host: process.env.DB_HOST,
     user:process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-});
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, 
+    idleTimeout: 60000,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+}).promise();
 
 export default conn;
