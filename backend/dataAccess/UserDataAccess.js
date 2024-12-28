e1e2405ad1821c6850ed17cb67caeece4c12c3da
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2024-12-28 20:41:47 +0200

    POST /user-api/user - Insert user in DB

--- backend/dataAccess/UserDataAccess.js
+++ backend/dataAccess/UserDataAccess.js
@@ -3,7 +3,7 @@ import conn from "../dbConfig.js";
 
 /**
  * If ORM fails, call this as getUser(false), as it will switch to the default MySQL promise-based pool
- * 
+ * @route GET /user-api/user
  * @param {type: boolean} ORM true = use Sequelize ORM, false = use MySQL pool 
  * @returns {type: any[]} Array with all the users in the DB.
  */
@@ -19,6 +19,28 @@ async function getUser(ORM = true) {
     }
 }
 
+/**
+ * Use JSON.stringify() in React to provide consistent data.
+ * @route POST /user-api/user with body
+ * @param {type: JSON { name: " ", email: " ", password: " ", role: " "}} user 
+ * @param {type: boolean} ORM 
+ * @returns User inserted in the database.
+ */
+
+async function createUser(user, ORM = true) {
+    if(!ORM) { 
+        const sql = "INSERT INTO Users SET ?";
+        const [result] = await conn.query(sql, user);
+
+        // Return data similar to ORM result:
+        return {user_id: result.insertId, ...user};
+    }
+    else {
+        return await User.create(user);
+    }
+}
+
 export {
-    getUser
+    getUser,
+    createUser
 }
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getUser } from "../dataAccess/UserDataAccess.js";
+import { createUser, getUser } from "../dataAccess/UserDataAccess.js";
 
 const usersRouter = express.Router();
 
 usersRouter.route('/user')
     .get(async (req, res) => { res.status(200).json(await getUser())});
+
+usersRouter.route('/user')
+    .post(async (req, res) => { res.status(201).json(await createUser(req.body, false))});
 
 export default usersRouter;
