d1c06335cc660c0309799e0f3239fb240cc20fe7
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2024-12-29 18:21:20 +0200

    DMOTEAM-9 Fully working login & register

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -1,47 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { login, getUserAuthStatus, getCurrentAuthenticatedUser } from '../services/userService';
 
-const login = () => {
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const EXPIRY_TIME_HOURS = 3;
+
+    let logged_in_user = null;
+
+    const onFormSubmit = async (event) => { 
+        event.preventDefault();
+        
+        if(email && password) {
+            const response = await login(email, password);
+            
+            if(response.data){
+                logged_in_user = response.data.user;
+            }
+        }
+
+        if(logged_in_user) {
+            let currentDate = new Date();
+            const expiryDate = new Date(currentDate);
+            expiryDate.setHours(currentDate.getHours() + EXPIRY_TIME_HOURS);
+
+            const authData = {
+                user: logged_in_user,
+                expiresAt: expiryDate.toISOString()
+            }
+
+            localStorage.setItem("authData", JSON.stringify(authData));
+        }
+
+        setTimeout(()=>{
+            const user = getCurrentAuthenticatedUser();
+            if(user){
+                console.log("authenticated user: ");
+                console.log(user);
+            }
+        }, 2000)
+    }
+
   return (
   <div className='h-screen w-screen flex items-center justify-center '>
-    <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-      <h1 class="text-2xl font-bold text-gray-700 text-center mb-6">Log in</h1>
-      <form action="#" method="POST">
-        <div class="mb-4">
-          <label for="email" class="block text-sm font-medium text-gray-600 mb-1">Email</label>
+    <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+      <h1 className="text-2xl font-bold text-gray-700 text-center mb-6">Log in</h1>
+      <form onSubmit={onFormSubmit}>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-600 mb-1">Email</label>
           <input 
             type="email" 
             id="email" 
             name="email" 
             placeholder="Enter your email" 
+            defaultValue={email}
+            onChange={(event)=>{setEmail(event.target.value)}}
             required 
-            class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
           />
         </div>
-        <div class="mb-6">
-          <label for="password" class="block text-sm font-medium text-gray-600 mb-1">Password</label>
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-sm font-medium text-gray-600 mb-1">Password</label>
           <input 
             type="password" 
             id="password" 
             name="password" 
-            placeholder="Enter your password" 
+            placeholder="Enter your password"
+            defaultValue={password} 
+            onChange ={(event)=>{setPassword(event.target.value)}}
             required 
-            class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
           />
         </div>
         <button 
-          type="submit" 
-          class="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition-colors"
+          type="submit"
+          className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition-colors"
         >
           Login
         </button>
       </form>
-      <p class="text-sm text-gray-600 text-center mt-4">
+      <p className="text-sm text-gray-600 text-center mt-4">
         Don't have an account? 
-        <a href="/signup" class="text-blue-500 hover:underline">Sign up</a>
+        <a href="/signup" className="text-blue-500 hover:underline">Sign up</a>
       </p>
     </div>
   </div>
   )
 }
 
-export default login
+export default Login
