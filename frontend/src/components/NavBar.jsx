c5979afcafcf41734c4585d24e8bc856c9e9bc7a
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-01-03 19:41:25 +0200

    NavBar - Navigate to "/" on logo clicked

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <div className='w-full h-[80px] bg-gray-100 flex items-center justify-between px-8 fixed'> 
       <div className='flex items-center'>
-        <img src="/svg/articles.svg" height={50} width={50} alt='logo' />
+        <img src="/svg/articles.svg" height={50} width={50} alt='logo' onClick={()=>{window.location.href="/"}} style={{cursor: 'pointer'}}/>
         <p className='text-xl font-bold text-gray-700'>Review Conference Platform</p>
       </div>
       <div className='flex items-center bg-gray-200 rounded-lg px-3 py-1 gap-2 cursor-pointer'>
