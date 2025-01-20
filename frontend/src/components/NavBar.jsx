7cb6155fafb25fc5cf50478e5233c21518bf3061
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-01-20 14:02:48 +0200

    Nav bar logout on user name click

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getCurrentAuthenticatedUser } from '../services/userService';
+import { getCurrentAuthenticatedUser, logout } from '../services/userService';
 
 
 const NavBar = () => {
@@ -10,10 +10,27 @@ const NavBar = () => {
         <img src="/svg/articles.svg" height={50} width={50} alt='logo' onClick={()=>{window.location.href="/"}} style={{cursor: 'pointer'}}/>
         <p className='text-xl font-bold text-gray-700'>Review Conference Platform</p>
       </div>
+      {
+      !user && 
       <a className='flex items-center bg-gray-200 rounded-lg px-3 py-1 gap-2 cursor-pointer' href="/login">
         <span className="material-symbols-outlined text-[50px] text-gray-700">account_circle</span>
         <span className='text-grxlay-700 text-'>{user ? user.name : "Log in"}</span>
-      </a>
+      </a> 
+      }
+
+      {
+        user && 
+        <a className='flex items-center bg-gray-200 rounded-lg px-3 py-1 gap-2 cursor-pointer' 
+            onClick={()=> 
+            {
+                logout();
+                window.location.reload();
+            }}>
+            <span className="material-symbols-outlined text-[50px] text-gray-700">account_circle</span>
+            <span className='text-grxlay-700 text-'>{user ? user.name : "Log in"}</span>
+        </a>
+      }
+      
         
     </div>
   )
